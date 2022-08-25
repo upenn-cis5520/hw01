e6401dc21cca70222999ce322ae501d1273c731e
Stephanie Weirich <sweirich@seas.upenn.edu>
2022-08-25 10:30:08 -0400

    22fa

---
+++ src/Main.hs
@@ -0,0 +1,23 @@
+module Main where
+
+import HW01 
+import Test.HUnit ( runTestTT, Test(TestList) )
+
+{-
+
+This module is the "entry point" for this assignment and runs the tests 
+for each homework problem. You should not edit this definition. 
+Instead, your goal is to complete each problem so that all of the tests pass. 
+
+-}
+
+main :: IO ()
+main = do
+    putStrLn "Runing test cases for HW01 ..."
+    _ <- runTestTT $ TestList [ testStyle,
+                                testLists,
+                                testHO,
+                                testFoldr,
+                                testWeather,
+                                testSoccer ]
+    return ()
